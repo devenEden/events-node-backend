063a66a74b7c269772b47aea9bba36424e2eb702
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2023-01-13 11:01:18 +0300

    Workable staff controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const EventsController = require("./events/events.controller");
 const UserController = require("./users/users.controller");
+const StaffController = require("./staff/staff.controller");
 
 module.exports = {
   UserController,
   EventsController,
+  StaffController,
 };
