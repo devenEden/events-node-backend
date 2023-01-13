197964a53296acb63958f3e8760392c80c7ee2bc
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2023-01-13 16:20:30 +0300

    Event activities controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const EventsController = require("./events/events.controller");
 const UserController = require("./users/users.controller");
 const StaffController = require("./staff/staff.controller");
+const EventActivitiesController = require("./events/eventActivities.controller");
 
 module.exports = {
   UserController,
   EventsController,
   StaffController,
+  EventActivitiesController,
 };
