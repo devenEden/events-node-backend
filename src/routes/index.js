514cd98434aea47e669888a60472b2582df55b17
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2023-01-13 16:21:01 +0300

    Event activities routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const loginRequired = require("../middleware/authRoute");
 const eventsRouter = require("./events/events.routes");
 const usersRouter = require("./users/users.routes");
 const staffRouter = require("./staff/staff.routes");
+const eventActivitiesRouter = require("./events/eventActivities.routes");
 const router = express.Router();
 
 /* GET home page. */
@@ -14,5 +15,6 @@ router.get("/", (req, res) => {
 router.use(`${prefix}/users`, usersRouter);
 router.use(`${prefix}/events`, loginRequired, eventsRouter);
 router.use(`${prefix}/staff`, loginRequired, staffRouter);
+router.use(`${prefix}/event-activities`, loginRequired, eventActivitiesRouter);
 
 module.exports = router;
